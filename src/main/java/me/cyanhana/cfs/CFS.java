f1e979c57ab61b46a86e1479805fa037d9b66319
Cyanhana_Neko <111105781+RunrunXi@users.noreply.github.com>
2026-06-28 02:24:58 +0800

    第一次提交

---
+++ src/main/java/me/cyanhana/cfs/CFS.java
@@ -0,0 +1,13 @@
+package me.cyanhana.cfs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CFS.MODID)
+public class CFS {
+    public static final String MODID = "cfs";
+
+    public CFS() {
+
+    }
+
+}
